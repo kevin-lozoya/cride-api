ccbd4278d67df4e661205df4aa3c99a4b8be41e0
Kevin Andrés Lozoya Giner <kalozoya@gregal.info>
2019-02-01 12:44:03 +0100

    Organizar modelos en paquetes de Django.

--- cride/users/models/users.py
+++ cride/users/models/users.py
@@ -1,7 +1,8 @@
-"""Users models."""
+"""User model."""
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 from cride.utils.models import CRideModel
 
@@ -16,12 +17,21 @@ class User(CRideModel, AbstractUser):
     email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'A user with that email already exists.'
         }
     )
+    
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone number must be entered in the format: +99999999. Up to 15 digits alloweb.'
+    )
 
-    phone_number = models.CharField(max_length=17, black=True)
+    phone_number = models.CharField(
+        validators=[phone_regex], 
+        max_length=17, 
+        blank=True
+    ) 
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
@@ -40,3 +50,12 @@ class User(CRideModel, AbstractUser):
         default=False,
         help_text='Set to true when the user have verified its email address.'
     )
+
+    def __str__(self):
+        """Return username."""
+
+        return self.username
+
+    def get_short_name(self):
+        """Return username."""
+        return self.username
