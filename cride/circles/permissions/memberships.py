308f56d0ba9c7d090413fc2205f2498b8fd1b2e4
Kevin Andrés Lozoya Giner <kalozoya@gregal.info>
2019-09-02 17:23:06 +0200

    Unirse a grupo.

--- cride/circles/permissions/memberships.py
+++ cride/circles/permissions/memberships.py
@@ -15,7 +15,7 @@ class IsActiveCircleMember(BasePermission):
     """
 
     def has_permission(self, request, view):
-        """Vefir user si a n active member of the circle."""
+        """Vefiry user is an active member of the circle."""
         
         try:
             Membership.objects.get(
@@ -23,6 +23,20 @@ class IsActiveCircleMember(BasePermission):
                 circle=view.circle,
                 is_active=True
             )
-        except Membership.DoesNotExiste:
+        except Membership.DoesNotExist:
             return False
         return True
+
+
+class IfSelfMember(BasePermission):
+    """Allow access only to member owners."""
+
+    def has_permission(self, request, view):
+        """Let object permisssion grant access."""
+        obj = view.get_object()
+        return self.has_object_permission(request, view, obj)
+
+    def has_object_permission(self, request, view, obj):
+        """Allow access only if member is owned by the requesting user."""
+        return request.user == obj.user
+
--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -1,7 +1,7 @@
 """Circle membership views."""
 
 # Django REST Framework
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -11,13 +11,14 @@ from cride.circles.models import Circle, Membership, Invitation
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
-from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.circles.permissions.memberships import IsActiveCircleMember, IfSelfMember
 
 # Serializers
-from cride.circles.serializers import MembershipModelSerializer
+from cride.circles.serializers import AddMemberSerializer, MembershipModelSerializer
 
 
 class MembershipViewSet(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
                         mixins.RetrieveModelMixin,
                         mixins.DestroyModelMixin,
                         viewsets.GenericViewSet):
@@ -35,7 +36,11 @@ class MembershipViewSet(mixins.ListModelMixin,
     def get_permissions(self):
         """Assign permission based on action."""
 
-        permissions = [IsAuthenticated, IsActiveCircleMember]
+        permissions = [IsAuthenticated]
+        if self.action != 'create':
+            permissions.append(IsActiveCircleMember)
+        if self.action == 'invitations':
+            permissions.append(IfSelfMember)
         return [p() for p in permissions]
 
     def get_queryset(self):
@@ -70,27 +75,27 @@ class MembershipViewSet(mixins.ListModelMixin,
         used its invitations and another list containing the
         invitations that haven't being used yet.
         """
-        
+
         member = self.get_object()
 
         invited_members = Membership.objects.filter(
             circle=self.circle,
-            invited_by=request.user,
+            invited_by=member.user,
             is_active=True
         )
 
         unused_invitations = Invitation.objects.filter(
             circle=self.circle,
-            issued_by=request.user,
+            issued_by=member.user,
             used=False
         ).values_list('code')
         diff = member.remaining_invitations - len(unused_invitations)
-        
+
         invitations = [x[0] for x in unused_invitations]
         for i in range(0, diff):
             invitations.append(
                 Invitation.objects.create(
-                    issued_by=request.user,
+                    issued_by=member.user,
                     circle=self.circle
                 ).code
             )
@@ -102,4 +107,14 @@ class MembershipViewSet(mixins.ListModelMixin,
         }
         return Response(data)
 
-
+    def create(self, request, *args, **kwargs):
+        """Handle member creation from invitation code."""
+        serializer = AddMemberSerializer(
+            data=request.data,
+            context={'circle': self.circle, 'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        member = serializer.save()
+        data = self.get_serializer(member).data
+        headers = self.get_success_headers(data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
