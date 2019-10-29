fac3378076fda24b407622fb7d6d03884ff623fd
Kevin Lozoya <kevinlozoyaginer@gmail.com>
2019-10-29 12:17:36 +0100

    Creando tarea asíncrona

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -3,10 +3,7 @@
 # Django
 from django.conf import settings
 from django.contrib.auth import authenticate, password_validation
-from django.core.mail import EmailMultiAlternatives
 from django.core.validators import RegexValidator
-from django.template.loader import render_to_string
-from django.utils import timezone
 
 # Django REST Framework
 from rest_framework import serializers
@@ -16,12 +13,11 @@ from rest_framework.validators import UniqueValidator
 # Models
 from cride.users.models import User, Profile
 
+# Tasks
+from cride.taskapp.tasks import send_confirmation_email
+
 # Serializers
 from cride.users.serializers.profiles import ProfileModelSerializer
-
-# Utilities
-from datetime import timedelta
-import jwt
 
 
 class UserModelSerializer(serializers.ModelSerializer):
@@ -71,7 +67,7 @@ class UserLoginSerializer(serializers.Serializer):
         return user, token.key
 
 
-class UserSignUpSerializer(serializers.Serializer):
+class UserSignUpSerializer(serializers.Serializer):  # pylint: disable=abstract-method
     """User sign up serializer.
     
     Handle sign up data validation and user/profile creation.
@@ -122,34 +118,8 @@ class UserSignUpSerializer(serializers.Serializer):
         data.pop('password_confirmation')
         user = User.objects.create_user(**data, is_verified=False, is_client=True)
         Profile.objects.create(user=user)
-        self.send_confirmation_email(user)
+        send_confirmation_email.delay(user_pk=user.pk)
         return user
-
-    def send_confirmation_email(self, user):
-        """Send account verification link to given user."""
-
-        verification_token = self.gen_verification_token(user)
-        subject = 'Welcome @{}! Verify your account to start using Comparte Ride'.format(user.username)
-        from_email = 'Comparte Ride <noreply@comparteride.com>'
-        content = render_to_string(
-            'emails/users/account_verification.html',
-            {'token': verification_token, 'user': user}
-        )
-        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
-        msg.attach_alternative(content, 'text/html')
-        msg.send()
-
-    def gen_verification_token(self, user):
-        """Create JWT token that the user can use to verify its account."""
-
-        exp_date = timezone.now() + timedelta(days=3)
-        payload = {
-            'user': user.username,
-            'exp': int(exp_date.timestamp()),
-            'type': 'email_confirmation'
-        }
-        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-        return token.decode()
 
 
 class AccountVerificationSerializer(serializers.Serializer):
